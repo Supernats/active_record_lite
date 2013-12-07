2456a3e1226146e87873ab9ba74ed8c52406ba23
App Academy Student <student@appacademy.io>
2013-12-07 13:18:05 -0800

    Remove debug printouts

--- new_attr_accessor.rb
+++ new_attr_accessor.rb
@@ -2,8 +2,6 @@ class Class
 
   def new_attr_accessor(*attributes)
     attributes.each do |attribute|
-      puts "ATTRIBUTE: "
-      p attribute
 
       self.send(:define_method, "#{attribute}") do
         self.instance_variable_get("@#{attribute.to_s}")
