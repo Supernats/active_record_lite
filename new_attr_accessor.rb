770d0085929d1f7dd2ce1b5d868c4f6a0713d616
App Academy Student <student@appacademy.io>
2013-12-07 13:17:38 -0800

    Finish new_attr_accessor

--- new_attr_accessor.rb
+++ new_attr_accessor.rb
@@ -1,14 +1,18 @@
-class Object
+class Class
 
-  def new_attr_accessor(*args)
-    args.each do |arg|
-      self.send(:define_method, "#{arg}") do
-        self.instance_variable_get(:@arg)
+  def new_attr_accessor(*attributes)
+    attributes.each do |attribute|
+      puts "ATTRIBUTE: "
+      p attribute
+
+      self.send(:define_method, "#{attribute}") do
+        self.instance_variable_get("@#{attribute.to_s}")
       end
 
-      self.send(:define_method, "#{arg}=()") do
-        self.instance_variable_set(:@arg, val)
+      self.send(:define_method, "#{attribute}=") do |val|
+        self.instance_variable_set("@#{attribute.to_s}", val)
       end
+
     end
     nil
   end
