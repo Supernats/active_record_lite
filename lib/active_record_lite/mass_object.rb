e3e6a7a52473554c46f43912f1a807e82c3291c1
App Academy Student <student@appacademy.io>
2013-12-07 18:11:11 -0800

    Implement SQL searches

--- lib/active_record_lite/mass_object.rb
+++ lib/active_record_lite/mass_object.rb
@@ -1,22 +1,41 @@
 class MassObject
+
   # takes a list of attributes.
   # adds attributes to whitelist.
   def self.my_attr_accessible(*attributes)
+    self.attributes.concat(attributes)
   end
 
   # takes a list of attributes.
   # makes getters and setters
   def self.my_attr_accessor(*attributes)
-  end
+    attributes.each do |attribute|
 
+      define_method(attribute) do
+        self.instance_variable_get("@#{attribute}")
+      end
+
+      define_method("#{attribute}=") do |val|
+        self.instance_variable_set("@#{attribute}", val)
+      end
+
+    end
+    nil
+  end
 
   # returns list of attributes that have been whitelisted.
   def self.attributes
+    @attributes ||= []
   end
 
   # takes an array of hashes.
   # returns array of objects.
   def self.parse_all(results)
+    objects = []
+    results.each do |hash|
+      objects << self.new(hash)
+    end
+    objects
   end
 
   # takes a hash of { attr_name => attr_val }.
@@ -24,5 +43,16 @@ class MassObject
   # if the key (attr_name) is in the whitelist, the value (attr_val)
   # is assigned to the instance variable.
   def initialize(params = {})
+
+    params.each do |attr_name, attr_value|
+      attr_name = attr_name.to_sym if attr_name.is_a?(String)
+      if self.class.attributes.include?(attr_name)
+        self.send("#{attr_name}=", attr_value)
+      else
+        raise "mass assignment to unregistered attribute not_protected"
+      end
+    end
+
   end
+
 end
--- lib/active_record_lite/sql_object.rb
+++ lib/active_record_lite/sql_object.rb
@@ -2,31 +2,71 @@ require_relative './associatable'
 require_relative './db_connection' # use DBConnection.execute freely here.
 require_relative './mass_object'
 require_relative './searchable'
+require 'active_support/inflector'
 
 class SQLObject < MassObject
   # sets the table_name
   def self.set_table_name(table_name)
+    @table_name = table_name.underscore
   end
 
   # gets the table_name
   def self.table_name
+    @table_name
   end
 
   # querys database for all records for this type. (result is array of hashes)
   # converts resulting array of hashes to an array of objects by calling ::new
   # for each row in the result. (might want to call #to_sym on keys)
   def self.all
+    rows = DBConnection.execute(<<-SQL,)
+      SELECT
+        "#{table_name}".*
+      FROM
+        "#{table_name}"
+      SQL
+    objects = []
+    rows.each do |hash|
+      objects << self.new(hash)
+    end
+    objects
   end
 
   # querys database for record of this type with id passed.
   # returns either a single object or nil.
   def self.find(id)
+    found_cats = DBConnection.execute(<<-SQL, :id => id)
+      SELECT
+        "#{table_name}".*
+      FROM
+        "#{table_name}"
+      WHERE
+        "#{table_name}".id = :id
+      LIMIT
+        1
+      SQL
+    return nil if found_cats.empty?
+    self.new(found_cats[0])
   end
 
   # executes query that creates record in db with objects attribute values.
   # use send and map to get instance values.
   # after, update the id attribute with the helper method from db_connection
   def create
+    attribute_values = self.attribute_values
+    attribute_names = self.instance_variables.join(", ")
+    question_string = (['?'] * attribute_values.count).join(", ")
+
+    # query = { :attr_values => *attribute_values
+    #           :question_marks => question_string
+    #         }
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        "#{table_name}" #{attribute_names}
+      VALUES
+        "#{question_string}"
+      SQL
+    self.id = DBConnection.last_insert_row_id
   end
 
   # executes query that updates the row in the db corresponding to this instance
@@ -40,5 +80,11 @@ class SQLObject < MassObject
 
   # helper method to return values of the attributes.
   def attribute_values
+    [].tap do |attribute_values|
+      self.instance_variables.each do |attr_name|
+        attribute_values.concat(self.send("#{attr_name}"))
+      end
+    end
   end
+
 end
